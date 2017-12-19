8f63f2b1a7c6d1bdac141c3d3ebe15abe06ad3d0
Felipe Pacheco <pachecopauluciof@gmail.com>
2017-12-19 02:56:13 -0200

    Criamos pageadmin e tpl admin

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace Hcode;
+
+
+class PageAdmin extends Page
+{
+	
+	function __construct($opt = array(), $tpl_dir = "/views/admin/")
+	{
+
+		parent::__construct($opt, $tpl_dir);
+		
+	}
+}
+
+
+
+ ?>
